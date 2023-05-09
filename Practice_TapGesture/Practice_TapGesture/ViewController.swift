4ed36de0e9bf67e25a157ec1dde4a262c8350011
bricksky <skydh1214@naver.com>
2023-05-10 03:39:08 +0900

    부스트코스 실습

--- Practice_TapGesture/Practice_TapGesture/ViewController.swift
+++ Practice_TapGesture/Practice_TapGesture/ViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+//        let tapGesture: UITapGestureRecognizer =
+//        UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+//        self.view.addGestureRecognizer(tapGesture)
         // Do any additional setup after loading the view.
     }
 
